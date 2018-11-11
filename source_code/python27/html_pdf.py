f7bfd6ec768cd39e68d22a5b6418e73229b9ed39
Jingdong <yangjingdong2008@163.com>
2018-11-12 07:51:36 +0800

    PDF support for Chinese

--- source_code/python27/html_pdf.py
+++ source_code/python27/html_pdf.py
@@ -1,8 +1,30 @@
+#!/usr/bin/python
+#coding=utf-8
+
 import pdfkit
 
-path_wk = r'C:\tools\wkhtmltox\bin\wkhtmltopdf.exe'
+options = {
+    'page-size': 'Letter',
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in',
+    'encoding': "UTF-8",
+    'custom-header' : [
+        ('Accept-Encoding', 'gzip')
+    ],
+    'cookie': [
+        ('cookie-name1', 'cookie-value1'),
+        ('cookie-name2', 'cookie-value2'),
+    ],
+    'no-outline': None
+}
+
+path_wk = r'C:\Python27\wkhtmltox\bin\wkhtmltopdf.exe'
 config = pdfkit.configuration(wkhtmltopdf = path_wk)
 #pdfkit.from_url(url, r'D:\are you coding\pdf\taobao.pdf', configuration=config)
 
-
-pdfkit.from_string('Hello!','out.pdf', configuration=config)
+my_Son = u"杨宇哲真是个好孩子，有个好妈妈好爸爸!"
+pdfkit.from_string(my_Son,'out.pdf', configuration=config, options=options)
+my_wife = u"我有个好老婆"
+pdfkit.from_string(my_wife,'out.pdf', configuration=config, options=options)
